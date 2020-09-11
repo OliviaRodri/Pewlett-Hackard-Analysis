1c18c917f4da4bd6833c1cd7d1bac5012592eb4b
Olivia Rodriguez <obrodri@gmail.com>
2020-09-11 01:09:55 -0500

    added challenge file layout readme updated module finishes

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Pewlett-Hackard-Analysis
+The written analysis has the following structure, organization, and formatting:
+
+There is a title, and there are multiple sections. (2 pt)
+Each section has a heading and subheading. (2 pt)
+Links to images are working and displayed correctly. (2 pt)
+Analysis (14 points)
+The written analysis has the following:
+
+Overview of the analysis:
+
+The purpose of the new analysis is well defined. (3 pt)
+Results:
+
+There is a bulleted list with four major points from the two analysis deliverables. (6 pt)
+Summary:
+
+The summary addresses the two questions and contains two additional queries or tables that may provide more insight. (5 pt)
+Submission
+Once you’re ready to submit, make sure to check your work against the rubric to ensure you are meeting the requirements for this Challenge one final time. It’s easy to overlook items when you’re in the zone!
+
+As a reminder, the deliverables for this Challenge are as follows:
+
+Deliverable 1: The Number of Retiring Employees by Title
+Deliverable 2: The Employees Eligible for the Mentorship Program
+Deliverable 3: A written report on the employee database analysis (README.md)
+Upload the following to your Pewlett-Hackard-Analysis GitHub repository:
+
+The Queries folder with the Employee_Database_challenge.sql file
+The Data folder with the retirement_titles.csv, unique_titles.csv, retiring_titles.csv, and mentorship_eligibilty.csv files
+An updated README.md that has your written analysis
--- schema.sql
+++ schema.sql
@@ -61,3 +61,100 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 
 SELECT * FROM titles;
+
+-- Number of employees retiring
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name,
+emp_no, 
+INTO retirement_info
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+
+--7.3.3
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- List of managers per department
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+
