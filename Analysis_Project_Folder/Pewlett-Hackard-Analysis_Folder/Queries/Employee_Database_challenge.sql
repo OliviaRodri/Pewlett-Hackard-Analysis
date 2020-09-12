890707c045165679ee97a24495bb472da613ea90
Olivia Rodriguez <obrodri@gmail.com>
2020-09-12 13:29:23 -0500

    deliv 2 syntxs' complete

--- Analysis_Project_Folder/Pewlett-Hackard-Analysis_Folder/Queries/Employee_Database_challenge.sql
+++ Analysis_Project_Folder/Pewlett-Hackard-Analysis_Folder/Queries/Employee_Database_challenge.sql
@@ -35,6 +35,9 @@ ORDER BY COUNT DESC;
 DROP TABLE mentorship_eligibilty;
 
 -- Deliverable 2
+
+DROP TABLE mentorship_eligibilty
+
 SELECT DISTINCT ON (e.emp_no)  e.emp_no,
 e.first_name,
 e.last_name,
@@ -49,8 +52,8 @@ ON (e.emp_no = de.emp_no)
 INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
-
 
 SELECT * FROM mentorship_eligibilty
 
@@ -62,60 +65,3 @@ ORDER BY emp_no;
 
 
 SELECT * FROM titles
-
-DROP TABLE e_ti_w_duplicates
-
-DROP TABLE e_ti_unique
-
--- join empoyess and titles
-SELECT e.emp_no,
-e.first_name,
-e.last_name,
-e.birth_date,
-ti.title,
-ti.to_date
-INTO e_ti_w_duplicates
-FROM employees as e
-INNER JOIN titles as ti
-ON (e.emp_no = ti.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
-
--- get rid of duplicates
-SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-birth_date,
-title,
-to_date
-INTO e_ti_unique
-FROM e_ti_w_duplicates
-ORDER BY emp_no, to_date DESC;
-
-
--- join (empoyess and titles) and emp_dep
-SELECT e.emp_no,
-e.first_name,
-e.last_name,
-e.birth_date,
-de.from_date,
-de.to_date,
-e.title
-INTO mentorship_eligibilty_duplicates
-FROM e_ti_unique as e
-INNER JOIN dept_emp as de
-ON (e.emp_no = de.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no;
-
--- ger rid of duplicates
-SELECT (emp_no) emp_no,
-first_name,
-last_name,
-birth_date,
-from_date,
-to_date,
-title
---INTO mentorship_eligibilty_unique
-FROM mentorship_eligibilty_duplicates
-ORDER BY emp_no, to_date DESC;
